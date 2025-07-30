4fccbc28b9e3f962e231cf4cbdad3103803ef696
Alejandro Blasco <alebdm@icloud.com>
2025-07-30 16:53:34 +0200

    feat(emacs): Magit supporting bare repos
    My ~/.dotfiles is configured as a Git bare repository and here's a hack
    for `magit` to support them.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -193,6 +193,28 @@
 (after! eglot
   (set-eglot-client! 'cc-mode '("clangd" "-j=3" "--clang-tidy"))) ; Use clangd
 
+(after! magit
+  ;; Support Git bare repos
+  ;; https://github.com/magit/magit/issues/460#issuecomment-1475082958
+  (defun ~/magit-process-environment (env)
+    "Detect and set git -bare repo env vars when in tracked dotfile directories."
+    (let* ((default (file-name-as-directory (expand-file-name default-directory)))
+           (git-dir (expand-file-name "~/.dotfiles/"))
+           (work-tree (expand-file-name "~/"))
+           (dotfile-dirs
+            (-map (apply-partially 'concat work-tree)
+                  (-uniq (-keep #'file-name-directory
+                                (split-string (shell-command-to-string
+                                               (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
+                                                       git-dir work-tree))))))))
+      (push work-tree dotfile-dirs)
+      (when (member default dotfile-dirs)
+        (push (format "GIT_WORK_TREE=%s" work-tree) env)
+        (push (format "GIT_DIR=%s" git-dir) env)))
+    env)
+  (advice-add 'magit-process-environment
+              :filter-return #'~/magit-process-environment))
+
 (after! man
   ;; Make the manpage the current buffer in the other window (default friendly)
   ;; https://github.com/emacs-mirror/emacs/blob/master/lisp/man.el
