6dc542bf0d50fff097e6eb67e73414902ebb1768
Alejandro Blasco <alebdm@icloud.com>
2025-08-18 09:12:20 +0200

    chore(emacs): Arrange sections and add headings

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -9,6 +9,10 @@
 (setq! user-full-name "Alejandro Blasco"
        user-mail-address "alebdm@icloud.com")
 
+;;;
+;;; UI
+;;;
+
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
@@ -70,20 +74,6 @@
 
 (setq! doom-theme 'modus-operandi)
 
-;; Maximize Emacs frame on start-up
-;; https://emacs.stackexchange.com/questions/2999/how-to-maximize-my-emacs-frame-on-start-up
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
-;; Screen for dashboard
-(setq! fancy-splash-image (concat doom-private-dir "iamdoom.png"))
-;; Remove dashboard sections
-;; https://discourse.doomemacs.org/t/how-to-change-your-splash-screen/57#addremove-sections-of-the-dashboard-1
-(remove-hook! '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu #'doom-dashboard-widget-footer)
-
-;; Display window separator
-;; https://discourse.doomemacs.org/t/how-to-switch-customize-or-write-themes/37
-;;(custom-set-faces! '(vertical-border :foreground "gray"))
-
 ;; Relative line numbers for knowing how far away line numbers are,
 ;; then ESC 12 <j/k> gets you exactly where you think
 (setq! display-line-numbers-type 'relative)
@@ -91,25 +81,17 @@
 ;; Display vertical sroll bars
 (scroll-bar-mode t)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq! org-directory "~/org/")
+;; Maximize Emacs frame on start-up
+;; https://emacs.stackexchange.com/questions/2999/how-to-maximize-my-emacs-frame-on-start-up
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; Display window separator
+;; https://discourse.doomemacs.org/t/how-to-switch-customize-or-write-themes/37
+;(custom-set-faces! '(vertical-border :foreground "gray"))
 
 ;; Disable confirmation on exit
 ;; https://github.com/hlissner/doom-emacs/issues/2688
 (setq! confirm-kill-emacs nil)
-
-;; Disable hidding modeline in popups
-;; https://github.com/doomemacs/doomemacs/blob/master/modules/ui/popup/README.org#disabling-hidden-mode-line-in-popups
-(remove-hook '+popup-buffer-mode-hook #'+popup-set-modeline-on-enable-h)
-;; Popup rule for compilation buffers
-(set-popup-rule!
-  "^\\*compilation\\*"
-  :slot 1 :vslot -2 :size 0.3
-  :autosave t
-  :ttl nil
-  :quit nil    ; Dont quit on ESC
-  :select nil) ; Dont focus automatically
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -143,28 +125,16 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(after! doom-modeline
-  (setq! doom-modeline-persp-name t ; Display workspace (perpective) name
-         doom-modeline-major-mode-icon t)) ; Display major-mode icon
-;; Smaller font in modeline
-;; https://github.com/seagle0128/doom-modeline/blob/master/README.md#faq
-;; (setq! doom-modeline-height 1) ; Optional
-;; (if (facep 'mode-line-active)
-;;     (set-face-attribute 'mode-line-active nil :height 80) ; For 29+
-;;   (set-face-attribute 'mode-line nil :height 80))
-;; (set-face-attribute 'mode-line-inactive nil :height 80))
+;;;
+;;; Modules
+;;;
 
-;; Show modeline in eshell and vterm modes (hooks added in their own module's config)
-(after! eshell
-  (remove-hook! 'eshell-mode-hook #'hide-mode-line-mode))
-(after! vterm
-  (remove-hook! 'vterm-mode-hook #'hide-mode-line-mode))
-
+;;; Buit-ins
 (after! dired
   (setq! dired-kill-when-opening-new-dired-buffer t) ; Kill dired buffers immediately
-  ;; Ediff marked files from Dired
-  ;; https://oremacs.com/2017/03/18/dired-ediff/
+  ;; From https://oremacs.com/2017/03/18/dired-ediff/
   (defun ~/ediff-files-in-dired ()
+    "Ediff marked files from Dired"
     (interactive)
     (let ((files (dired-get-marked-files))
           (wnd (current-window-configuration)))
@@ -187,17 +157,44 @@
   (add-hook! 'dired-mode-hook
     (lambda () (define-key dired-mode-map (kbd "e") #'~/ediff-files-in-dired))))
 
-(after! cc-mode
-  ;; Underscore is part of the word in C buffers
-  ;; https://evil.readthedocs.io/en/latest/faq.html#underscore-is-not-a-word-character
-  (add-hook! 'c-mode-common-hook (lambda () (modify-syntax-entry ?_ "w"))))
 
-;; TODO not working
-(after! (:and cc-mode evil-nerd-commenter)
-  ;; C-style single line comments
-  (add-hook! 'c-mode-common-hook :append (lambda () (setq! comment-start "// "
-                                                           comment-end ""))))
+;;; :ui doom-dashboard
+;; Screen for dashboard
+(setq! fancy-splash-image (concat doom-private-dir "iamdoom.png"))
+;; Remove dashboard sections
+;; https://discourse.doomemacs.org/t/how-to-change-your-splash-screen/57#addremove-sections-of-the-dashboard-1
+(remove-hook! '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu #'doom-dashboard-widget-footer)
 
+;;; :ui modeline
+(after! doom-modeline
+  (setq! doom-modeline-buffer-file-name-style 'relative-to-project ; src/module/code.c
+         ;; doom-modeline-persp-name t ; Display workspace (perpective)
+         doom-modeline-major-mode-icon t) ; Display major-mode icon
+  ;; Doom adds a hook to enable filesize. I don't care
+  (remove-hook! 'doom-modeline-mode-hook #'size-indication-mode))
+;; Smaller font in modeline
+;; https://github.com/seagle0128/doom-modeline/blob/master/README.md#faq
+;; (setq! doom-modeline-height 1) ; Optional
+;; (if (facep 'mode-line-active)
+;;     (set-face-attribute 'mode-line-active nil :height 80) ; For 29+
+;;   (set-face-attribute 'mode-line nil :height 80))
+;; (set-face-attribute 'mode-line-inactive nil :height 80))
+
+
+;;: :ui popup
+;; Do not hide modeline in popups
+;; https://github.com/doomemacs/doomemacs/blob/master/modules/ui/popup/README.org#disabling-hidden-mode-line-in-popups
+(remove-hook '+popup-buffer-mode-hook #'+popup-set-modeline-on-enable-h)
+;; Popup rule for compilation buffers
+(set-popup-rule!
+  "^\\*compilation\\*"
+  :slot 1 :vslot -2 :size 0.3
+  :autosave t
+  :ttl nil
+  :quit nil    ; Dont quit on ESC
+  :select nil) ; Dont focus automatically
+
+;;; :ui tabs
 (after! centaur-tabs
   ;; Tabs style
   (setq! centaur-tabs-style "bar"
@@ -205,9 +202,31 @@
          centaur-tabs-height 13)
   (centaur-tabs-mode t))
 
+;;; :term
+;; Show modeline in eshell and vterm modes (hooks added in their own module's config)
+(after! eshell
+  (remove-hook! 'eshell-mode-hook #'hide-mode-line-mode))
+(after! vterm
+  (remove-hook! 'vterm-mode-hook #'hide-mode-line-mode))
+
+;;; :tools llm
+(after! gptel
+  ;; Set with: security add-generic-password -s perplexity-api -a alebdm -w
+  ;; API Keys: https://www.perplexity.ai/account/api/keys
+  (defun ~/get-llm-api-key ()
+    "Get Perplexity API key from macOS Keychain."
+    (let ((command "security find-generic-password -s perplexity-api -a alebdm -w"))
+      (replace-regexp-in-string "\n\\'" "" (shell-command-to-string command))))
+  (setq! gptel-model   'sonar
+         gptel-backend (gptel-make-perplexity "Perplexity"
+                         :key #'~/get-llm-api-key
+                         :stream t)))
+
+;;; :tools lsp +eglot
 (after! eglot
   (set-eglot-client! 'cc-mode '("clangd" "-j=3" "--clang-tidy"))) ; Use clangd
 
+;;; :tools magit
 (after! magit
   ;; Support Git bare repos
   ;; https://github.com/magit/magit/issues/460#issuecomment-1475082958
@@ -230,7 +249,21 @@
   (advice-add 'magit-process-environment
               :filter-return #'~/magit-process-environment))
 
+;;; :lang cc
+(after! cc-mode
+  ;; Underscore is part of the word in C buffers
+  ;; https://evil.readthedocs.io/en/latest/faq.html#underscore-is-not-a-word-character
+  (add-hook! 'c-mode-common-hook (lambda () (modify-syntax-entry ?_ "w"))))
+;; TODO not working
+(after! (:and cc-mode evil-nerd-commenter)
+  ;; C-style single line comments
+  (add-hook! 'c-mode-common-hook :append (lambda () (setq! comment-start "// "
+                                                           comment-end ""))))
 
+;;; :lang org
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq! org-directory "~/org/")
 (after! org
   (setq! org-startup-with-inline-images t) ; Display images
   ;; Some faces I like
@@ -238,8 +271,7 @@
                       :background (doom-color 'bg))
   (set-face-attribute 'org-block-end-line nil
                       :background (doom-color 'bg)))
-
-;; Pretty org
+;; +pretty
 (after! org-modern
   (setq! org-modern-star 'replace
          org-modern-replace-stars "◉○✸✧■□"
@@ -256,31 +288,30 @@
          org-modern-tag nil
          org-modern-block-name nil
          org-modern-keyword nil))
-
+;; +pretty
 (after! org-appear
   (setq! org-appear-autolinks 'just-brackets)) ; Show links with brackets around them. Hide link target
-
+;; +dragndrop
 (after! org-download
   (add-hook! 'dired-mode-hook 'org-download-enable)
   (setq! org-download-image-dir "~/org/pics/"
          org-download-screenshot-method "screencapture -i %s"))
+;; Roam UI
+(use-package! websocket
+  :after org-roam)
+(use-package! org-roam-ui
+  :after org-roam
+  ;; Normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;; a hookable mode anymore, you're advised to pick something yourself
+  ;; If you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq! org-roam-ui-sync-theme t
+         org-roam-ui-follow t
+         org-roam-ui-update-on-save t
+         org-roam-ui-open-on-start t))
 
-(after! gptel
-  ;; Function to retrieve the API key from macOS Keychain
-  ;; Set with: security add-generic-password -s perplexity-api -a alebdm -w
-  ;; API Keys: https://www.perplexity.ai/account/api/keys
-  (defun ~/get-llm-api-key ()
-    (let ((command "security find-generic-password -s perplexity-api -a alebdm -w"))
-      (replace-regexp-in-string "\n\\'" "" (shell-command-to-string command))))
-  (setq! gptel-model   'sonar
-         gptel-backend (gptel-make-perplexity "Perplexity"
-                         :key #'~/get-llm-api-key
-                         :stream t)))
-
-(after! elfeed
-  (setq! elfeed-search-filter "@1-week-ago +unread -rust -youtube -news -chatty")
-  (add-hook! 'elfeed-search-mode-hook #'elfeed-update)) ; Update feeds when opening
-
+;;; :email mu4e
 (after! mu4e
   (setq! mu4e-update-interval 300) ; Automatic fetching
   (setq! sendmail-program (executable-find "msmtp")
@@ -300,11 +331,18 @@
    t)
   (add-to-list 'mu4e-bookmarks
                '("maildir:/icloud/INBOX" "Inbox" ?i) t)
+
   (add-hook 'mu4e-compose-mode-hook
             (lambda ()
               (save-excursion (message-add-header "Cc:\n"))
               (save-excursion (message-add-header "Bcc:\n")))))
 
+;;; :app rss
+(after! elfeed
+  (setq! elfeed-search-filter "@1-week-ago +unread -rust -youtube -news -chatty")
+  (add-hook! 'elfeed-search-mode-hook #'elfeed-update)) ; Update feeds when opening
+
+;;; :config default
 ;; Always return `man' for documentation lookup
 (defun +default/man-or-woman ()
   (interactive)
@@ -313,6 +351,11 @@
   ;; Make the manpage the current buffer in the other window (default friendly)
   ;; https://github.com/emacs-mirror/emacs/blob/master/lisp/man.el
   (setq! Man-notify-method 'aggressive))
+
+;;;
+;;; Packages
+;;;
+
 (use-package! auto-dark
   ;; Based on https://github.com/LionyxML/auto-dark-emacs#doom-emacs
   ;; and https://github.com/doomemacs/doomemacs/issues/6424
@@ -321,6 +364,11 @@
   (auto-dark-allow-osascript t)
   (auto-dark-allow-powershell nil)
   (auto-dark-themes '((modus-vivendi) (modus-operandi))))
+
+
+(use-package! nyan-mode
+  :after doom-modeline
+  :init (nyan-mode))
 
 ;; Built-in Htop
 ;; https://laurencewarne.github.io/emacs/programming/2022/12/26/exploring-proced.html
@@ -338,27 +386,10 @@
    '(custom user pid ppid sess tree pcpu pmem rss start time state (args comm))))
 (defalias 'htop 'proced)
 
-(use-package! nyan-mode
-  :after doom-modeline
-  :init (nyan-mode))
+;;;
+;;; Mappings
+;;;
 
-
-
-(use-package! websocket
-  :after org-roam)
-(use-package! org-roam-ui
-  :after org-roam
-  ;; Normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-  ;; a hookable mode anymore, you're advised to pick something yourself
-  ;; If you don't care about startup time, use
-  ;;  :hook (after-init . org-roam-ui-mode)
-  :config
-  (setq! org-roam-ui-sync-theme t
-         org-roam-ui-follow t
-         org-roam-ui-update-on-save t
-         org-roam-ui-open-on-start t))
-
-;; Mappings
 (map! :leader
       ;; <leader> f a : Find alternate .h/.cpp file
       (:prefix-map ("f" . "file")
