5a3341e242b549cfd87d24e586a047be06eea057
Alejandro Blaco <alebdm@icloud.com>
2026-03-06 08:54:58 +0100

    feat(aerospace): Move to Nix
    Convert Aerospace's TOML configuration to Nix nested attribute:
    
    nix eval --impure --expr "builtins.fromTOML (builtins.readFile \"$HOME/.aerospace.toml\")"

--- home.nix
+++ home.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+
+  imports = [
+     ./modules/aerospace.nix
+  ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "alejandro";
---
+++ modules/aerospace.nix
@@ -0,0 +1,174 @@
+{ ... }:
+
+{
+  # macOS required settings
+  # https://nikitabobko.github.io/AeroSpace/guide.html#a-note-on-displays-have-separate-spaces
+  # https://nikitabobko.github.io/AeroSpace/guide.html#a-note-on-mission-control
+  targets.darwin.defaults = {
+    "com.apple.spaces" = {
+      "spans-displays" = true;    # One space spans all displays
+    };
+    "com.apple.dock" = {
+      "expose-group-apps" = true; # Group windows by app in Mission Control
+    };
+  };
+
+  programs.aerospace = {
+    enable = true;
+
+    launchd = {
+      enable    = true;
+      keepAlive = true;  # default, can omit
+    };
+
+    settings = {
+      accordion-padding = 30;
+
+      automatically-unhide-macos-hidden-apps = false;
+
+      default-root-container-layout      = "tiles";
+      default-root-container-orientation = "auto";
+
+      enable-normalization-flatten-containers                         = true;
+      enable-normalization-opposite-orientation-for-nested-containers = true;
+
+      # Mouse follow focus
+      on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
+
+      key-mapping.preset = "qwerty";
+
+      gaps = {
+        inner = {
+          horizontal = [ { monitor.built-in = 3; } 4 ];
+          vertical   = [ { monitor.built-in = 3; } 4 ];
+        };
+        outer = {
+          left   = [ { monitor.built-in = 0; } 0 ];
+          right  = [ { monitor.built-in = 0; } 0 ];
+          top    = [ { monitor.built-in = 0; } 0 ];
+          bottom = [ { monitor.built-in = 0; } 0 ];
+        };
+      };
+
+      workspace-to-monitor-force-assignment = {
+        "0-Code"   = [ "main" "built-in" ];
+        "1-Init"   = [ "built-in" "secondary" ];
+        "2-Comms"  = [ "built-in" "secondary" ];
+        "3-System" = [ "built-in" "secondary" ];
+        "4"        = [ "built-in" "secondary" ];
+        "5"        = [ "built-in" "secondary" ];
+        "6"        = [ "built-in" "secondary" ];
+        "7"        = [ "main" "built-in" ];
+        "8"        = [ "main" "built-in" ];
+        "9-Browse" = [ "main" "built-in" ];
+      };
+
+      mode = {
+        main.binding = {
+          # Workspace focus
+          alt-1 = "workspace 1-Init";
+          alt-2 = "workspace 2-Comms";
+          alt-3 = "workspace 3-System";
+          alt-4 = "workspace 4";
+          alt-5 = "workspace 5";
+          alt-6 = "workspace 6";
+          alt-7 = "workspace 7";
+          alt-8 = "workspace 8";
+          alt-9 = "workspace 9-Browse";
+          alt-0 = "workspace 0-Code";
+          # alt-q = 'workspace ●○○○'
+          # alt-w = 'workspace ○●○○'
+          # alt-e = 'workspace ○○●○'
+          # alt-r = 'workspace ○○○●'
+          alt-shift-h = "workspace --wrap-around prev";
+          alt-shift-l = "workspace --wrap-around next";
+          alt-tab     = "workspace-back-and-forth";
+
+          # Windows focus
+          alt-h = "focus --ignore-floating --boundaries-action wrap-around-the-workspace left";
+          alt-j = "focus --ignore-floating --boundaries-action wrap-around-the-workspace down";
+          alt-k = "focus --ignore-floating --boundaries-action wrap-around-the-workspace up";
+          alt-l = "focus --ignore-floating --boundaries-action wrap-around-the-workspace right";
+
+          # Windows movements
+          alt-ctrl-h = "move left";
+          alt-ctrl-j = "move down";
+          alt-ctrl-k = "move up";
+          alt-ctrl-l = "move right";
+          # To workspace
+          alt-ctrl-1 = "move-node-to-workspace 1-Init";
+          alt-ctrl-2 = "move-node-to-workspace 2-Comms";
+          alt-ctrl-3 = "move-node-to-workspace 3-System";
+          alt-ctrl-4 = "move-node-to-workspace 4";
+          alt-ctrl-5 = "move-node-to-workspace 5";
+          alt-ctrl-6 = "move-node-to-workspace 6";
+          alt-ctrl-7 = "move-node-to-workspace 7";
+          alt-ctrl-8 = "move-node-to-workspace 8";
+          alt-ctrl-9 = "move-node-to-workspace 9-Browse";
+          alt-ctrl-0 = "move-node-to-workspace 0-Code";
+          # To monitor
+          # Note: move-workspace-to-monitor has no effect if workspace-to-monitor-force-assignment
+          alt-ctrl-left  = "move-node-to-monitor --focus-follows-window prev";
+          alt-ctrl-right = "move-node-to-monitor --focus-follows-window next";
+          alt-ctrl-tab   = "move-workspace-to-monitor --wrap-around next";
+
+          # Windows joins
+          alt-ctrl-shift-h = "join-with left";
+          alt-ctrl-shift-j = "join-with down";
+          alt-ctrl-shift-k = "join-with up";
+          alt-ctrl-shift-l = "join-with right";
+
+          # Windows layouts
+          alt-slash  = "layout tiles horizontal vertical";
+          alt-comma  = "layout accordion horizontal vertical";
+          alt-f      = "fullscreen --no-outer-gaps";
+
+          # Launch terminal
+          #alt-enter = "exec-and-forget osascript -e '\n    tell application \"Terminal\"\n        do script\n        activate\n    end tell'\n    ";
+          alt-enter = "exec-and-forget osascript -e 'tell application \"Terminal\" to do script' -e 'tell application \"Terminal\" to activate'";
+          # Mode switches
+          alt-r               = "mode resize";
+          alt-shift-semicolon = "mode service";
+        };
+
+        resize.binding = {
+          h     = "resize smart -50";
+          j     = "resize smart-opposite -50";
+          k     = "resize smart-opposite +50";
+          l     = "resize smart +50";
+          minus = "resize smart -50";
+          equal = "resize smart +50";
+          esc   = "mode main";
+          q     = "mode main";
+        };
+
+        service.binding = {
+          r         = [ "flatten-workspace-tree" "mode main" ]; # Reset layout
+          f         = [ "layout floating tiling" "mode main" ]; # Toggle floating <-> tiling
+          backspace = [ "close-all-windows-but-current" "mode main" ];
+          esc       = [ "reload-config" "mode main" ];
+          q         = "mode main";
+        };
+      };
+
+      # Get app-id with: aerospace list-apps
+      on-window-detected = [
+        { "if".app-id = "com.apple.Notes";             run = "move-node-to-workspace 1-Init"; }
+        { "if".app-id = "com.apple.Music";             run = "move-node-to-workspace 1-Init"; }
+        { "if".app-name-regex-substring = "YT Music";  run = "move-node-to-workspace 1-Init"; }
+        { "if".app-id = "org.apple.mail";              run = "move-node-to-workspace 2-Comms"; }
+        { "if".app-name-regex-substring = "Messages";  run = "move-node-to-workspace 2-Comms"; }
+        { "if".app-id = "net.whatsapp.WhatsApp";       run = "move-node-to-workspace 2-Comms"; }
+        { "if".app-name-regex-substring = "FastS3";    run = "move-node-to-workspace 2-Comms"; }
+        { "if".app-id = "org.pqrs.Karabiner-Elements.Settings"; run = "move-node-to-workspace 3-System"; }
+        { "if".app-id = "com.apple.shortcuts";         run = "move-node-to-workspace 3-System"; }
+        { "if".app-id = "com.apple.Safari";            run = "move-node-to-workspace 9-Browse"; }
+        { "if".app-id = "org.gnu.Emacs";               run = "move-node-to-workspace 0-Code"; }
+        { "if".app-name-regex-substring = "AI Studio"; run = "move-node-to-workspace 0-Code"; }
+        { "if".app-id = "com.apple.systempreferences"; run = "layout floating"; }
+        { "if".app-id = "com.apple.finder";            run = "layout floating"; }
+        { "if".app-id = "com.apple.ActivityMonitor";   run = "layout floating"; }
+      ];
+    };
+  };
+}
