bc0690363118f167b1305a9733bda3028f7bfb7f
Alejandro Blaco <alebdm@icloud.com>
2026-04-08 15:15:28 +0200

    feat(nix): Add some darwin system defaults
    https://nix-darwin.github.io/nix-darwin/manual/

--- hosts/wacos/darwin.nix
+++ hosts/wacos/darwin.nix
@@ -25,11 +25,44 @@
 
   # Apple system domain
   system.defaults = {
-    NSGlobalDomain.KeyRepeat = 2;
-    NSGlobalDomain.InitialKeyRepeat = 15;
-    NSGlobalDomain.ApplePressAndHoldEnabled = false;
-    dock.autohide = true;
-    finder.AppleShowAllExtensions = true;
-    screencapture.location = "~/Desktop";
+    # dock
+    dock.autohide = true;          # default: false
+    dock.autohide-delay = 0.0;     # default: 0.5
+    dock.show-recents = false;     # default: true
+    dock.mru-spaces = false;       # dont reorder spaces by most-recent-used; default: true
+    dock.expose-group-apps = true; # group windows by application in Mission Control; default: false
+    dock.tilesize = 64;            # default: 64
+
+    # spaces
+    spaces.spans-displays = true; # separate spaces for each display; default: null
+
+    # finder
+    finder.AppleShowAllExtensions = true;          # default: false
+    finder.AppleShowAllFiles = true;               # default: false
+    finder.ShowPathbar = true;                     # default: false
+    #finder.ShowStatusBar = true;                  # default: false
+    finder._FXSortFoldersFirst = true;             # default: false
+    finder.FXEnableExtensionChangeWarning = false; # default: true
+    finder.FXDefaultSearchScope = "SCsp";          # use previous seach scope; default: "SCev" (This Mac)
+    finder.FXPreferredViewStyle = "Nlsv";          # list view; default: "icnv" (icon view)
+
+    # NSGlobalDomain
+    NSGlobalDomain.KeyRepeat = 2;                                # default: 6
+    NSGlobalDomain.InitialKeyRepeat = 15;                        # default: 25
+    NSGlobalDomain.ApplePressAndHoldEnabled = false;             # default: true (accent menu displayed)
+    NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;     # default: true
+    NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;   # default: true
+    NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false; # default: true
+    NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;  # default: true
+    #NSGlobalDomain."com.apple.swipescrolldirection" = false;    # default: true
+    NSGlobalDomain.AppleICUForce24HourTime = true;               # default: false
+
+    # trackpad
+    trackpad.TrackpadThreeFingerDrag = true; # default: false
+    #trackpad.Clicking = true;               # default: false
+    trackpad.TrackpadRightClick = true;      # default: false
+
+    # login
+    loginwindow.GuestEnabled = false; # default: true
   };
 }
