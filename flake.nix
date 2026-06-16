d22f87c009a602045d7077d584ba0f0c8342e5dc
Alejandro Blasco <alebdm@icloud.com>
2026-06-16 11:44:40 +0200

    bump: back to unstable
    Emacs on aarch64-darwin in nixpkgs master builds again

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Home Manager configuration of alejandro";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
